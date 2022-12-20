68677a69c07692c71c01310ec6e6e40792c1ad37
NaokiTakahashi <naoki.takahashi060@gmail.com>
2022-12-20 22:43:37 +0900

    Add register-python-argcomplete to setup script

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,3 +1,11 @@
 script_patth=$(realpath "${BASH_SOURCE[0]}")
 this_script_dir=$(dirname "$script_patth")
-[ -d $this_script_dir ] && export PATH="$PATH:$this_script_dir"
+
+if [ -d $this_script_dir ]
+then
+    export PATH="$PATH:$this_script_dir"
+    if type "register-python-argcomplete" > /dev/null 2>&1
+    then
+        eval "$(register-python-argcomplete rose)"
+    fi
+fi
