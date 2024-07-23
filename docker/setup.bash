4e38a0aa77caa2d673d703d96ee7753d70c804c7
Naoki Takahashi <naoki.takahashi060@gmail.com>
2024-07-23 22:52:43 +0900

    Add rose-0.1 dockerfiles

---
+++ docker/setup.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+. /opt/pylocal/bin/activate
+. /opt/ros/${ROS_DISTRO}/setup.bash
+
+if [ -f "${ROSE_WORKSPACE}/builtin/install/setup.bash" ] 
+then
+    . ${ROSE_WORKSPACE}/builtin/install/setup.bash
+fi
+if [ -f "${ROSE_WORKSPACE}/builtin/gz_sim/install/setup.bash" ] 
+then
+    . ${ROSE_WORKSPACE}/builtin/gz_sim/install/setup.bash
+fi
+if [ -f "${ROSE_WORKSPACE}/install/setup.bash" ] 
+then
+    . ${ROSE_WORKSPACE}/install/setup.bash
+fi
+
+if [ -z "$RMW_IMPLEMENTATION" ]
+then
+    export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+    if [ -f "${ROSE_WORKSPACE}/profiles/rmw_cyclonedds_profile.xml" ]
+    then
+        export CYCLONEDDS_URI=${ROSE_WORKSPACE}/profiles/rmw_cyclonedds_profile.xml
+    fi
+fi
+
+if [ -z "$RCUTILS_COLORIZED_OUTPUT" ]
+then
+    export RCUTILS_COLORIZED_OUTPUT=1
+fi
+if [ -z "$ROS_DOMAIN_ID" ]
+then
+    export ROS_DOMAIN_ID=22
+fi
+if [ -z "$ROS_LOCALHOST_ONLY" ]
+then
+    export ROS_LOCALHOST_ONLY=0
+fi
